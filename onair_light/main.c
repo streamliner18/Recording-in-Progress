5fdedb786d0f2954d137c2e4be5ab0369de3d9ac
StreamLiner <jimmy.xrail@me.com>
2015-01-13 23:29:06 +0800

    Power saving update

--- onair_light/main.c
+++ onair_light/main.c
@@ -2,6 +2,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <avr/sleep.h>
 
 
 #define led_ind PB4
@@ -15,27 +16,45 @@ uint8_t led_on = 0;
 void initIO(void) {
     DDRB = 0b00011101;
     PORTB |= (1 << main_switch);
+    GIMSK |= (1 << INT0);
+    MCUCR &= ~(1 << ISC00);
+    MCUCR &= ~(1 << ISC01);//enable interrupt
+    sei();
+    //MCUCR |= (1 << SM0);
+    //MCUCR &= (1 << SM1); //deep sleep mode
+    MCUCR |= (1 << SE);
+    SLEEP_MODE_PWR_DOWN;
+    sleep_cpu();
 }
 
 int main(void) {
 	initIO();
     while (1) {
-        if (!(PINB & _BV(main_switch))) {
-            led_on = 1 - led_on;
-            if (led_on) {
-                PORTB |= 1 << main_p;
-                PORTB |= 1 << led_ind;
-                PORTB |= 1 << buzzer;
-                _delay_ms(400);
-                PORTB &= ~(1 << buzzer);
-            } else {
-                PORTB &= ~(1 << main_p);
-                PORTB &= ~(1 << led_ind);
-                PORTB |= 1 << buzzer;
-                _delay_ms(200);
-                PORTB &= ~(1 << buzzer);
+        /*if (!(PINB & _BV(main_switch))) {
+            
             }
-        }
+        }*/
 	}
 	return 0; // never reached
+}
+
+ISR(INT0_vect){
+    cli();
+    led_on = 1 - led_on;
+    if (led_on) {
+        PORTB |= 1 << main_p;
+        PORTB |= 1 << led_ind;
+        PORTB |= 1 << buzzer;
+        _delay_ms(400);
+        PORTB &= ~(1 << buzzer);
+    } else {
+        PORTB &= ~(1 << main_p);
+        PORTB &= ~(1 << led_ind);
+        PORTB |= 1 << buzzer;
+        _delay_ms(200);
+        PORTB &= ~(1 << buzzer);
+    }
+    sei();
+    sleep_enable();
+    sleep_cpu();
 }
